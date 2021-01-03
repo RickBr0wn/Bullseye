f7dc88d383ae444feb35bb0dfa582daa848c14a2
Rick Brown <ricky.brown.00@gmail.com>
2021-01-03 11:55:08 +0000

    adds a programming todo list

---
+++ Bullseye/TodoList.swift
@@ -0,0 +1,26 @@
+//
+//  TodoList.swift
+//  Bullseye
+//
+//  Created by Rick Brown on 03/01/2021.
+//
+
+/**
+ 
+ MUST HAVES
+  - Add an instructions label
+  - Add a target label
+  - Add a slider (values 1 - 100)
+  - Add a 'Hit Me' button
+  - Style the text
+  - Show a pop up when the button is pressed
+  - Read the value of the slider when the button is pressed
+  - Generate a random number
+  - Calculate the score
+
+ NICE TO HAVE
+  - Implement multiple rounds
+  - Implement restarting the game
+  - Add a leaderboard screen
+  - Make the app look pretty
+ */
